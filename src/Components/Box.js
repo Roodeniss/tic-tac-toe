17b0cc14f088cccc3a46c524c9e260a38d460d48
Roodeniss <66141538+Roodeniss@users.noreply.github.com>
2023-03-07 17:56:30 -0300

    Update del codigo y css
    Se quitan comentarios y se empieza con el css para mejorar experiencia de usuario

--- src/Components/Box.js
+++ src/Components/Box.js
@@ -3,17 +3,6 @@ import Title from "../Components/Title"
 import Board from "../Components/Board"
 
 export default function Box(props) {
-    // const [on, setOn] = React.useState(props.on)
-
-    // const styles = {
-    //     backgroundColor: on ? "#222222" : "transparent",
-
-    // }
-
-    // function toggle() {
-    //     setOn(prevOn => !prevOn)
-    // }
-
     function reload(){
         window.location.reload();
     }
@@ -22,22 +11,11 @@ export default function Box(props) {
         <>
             
             <Board />
-            {/* <div style={styles} className="box" onClick={toggle}></div>
-            <style >{`
-            .box{
-            display: grid;
-            grid-template-columns: repeat(3, 1fr);
-            grid-template-rows: repeat(3, 1fr);
-            height: 100px;
-            width: 100px;
-            border: 1px solid black;
-            margin-right: 4px;
-            border-radius: 5px;
-            }
-            `}
-            </style> */}
             <button className="reset" onClick={reload}>Reiniciar</button>
             <style>{`
+                *{
+                    background-color: #2a3b47;
+                }
                 .reset{
                     margin-top: 20%;
                     margin-left: 5%;
