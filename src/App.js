e9989e694e775639f8e15f54add68b13c3ed25aa
Roodeniss <66141538+Roodeniss@users.noreply.github.com>
2023-03-09 18:29:04 -0300

    Update code
    se eliminaron comentarios no necesarios

--- src/App.js
+++ src/App.js
@@ -2,15 +2,8 @@ import React from "react"
 import Main from "./Components/Main"
 
 export default function App() {
-    // const [squares, setSquares] = React.useState(boxes)
-    
-    // const squareElements = squares.map(square => (
-    //     <Box key={square.id} on={square.on} />
-    // ))
-    
     return (
         <main>
-            {/* {squareElements} */}
             <Main/>
         </main>
     )
